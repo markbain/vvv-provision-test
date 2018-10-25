63e515483cb78140303a0a5fbac6a8da4062c38d
markbain <markbaindesign@gmail.com>
2018-10-25 12:22:46 +0200

    use config file to set web root variable in multiple files

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 # Provision WordPress Stable
 
+source "${VVV_PATH_TO_SITE}/provision/custom-provision.conf"
+
+# Configure WP-CLI
+echo -e "\nCreating or updating 'wp-cli.yml'"
+cp -f "${VVV_PATH_TO_SITE}/wp-cli.yml.tmpl" "${VVV_PATH_TO_SITE}/wp-cli.yml"
+sed -i "s#{{WEBROOT_HERE}}#${WEBROOT}#" "${VVV_PATH_TO_SITE}/wp-cli.yml"
+
 DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
 DOMAINS=`get_hosts "${DOMAIN}"`
 SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
@@ -8,7 +15,6 @@ WP_VERSION=`get_config_value 'wp_version' 'latest'`
 WP_TYPE=`get_config_value 'wp_type' "single"`
 DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
 DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
-WEBROOT="public_html"
 
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
@@ -30,8 +36,9 @@ fi
 if [[ ! -f "${VVV_PATH_TO_SITE}/${WEBROOT}/wp-config.php" ]]; then
   echo "Configuring WordPress Stable..."
   noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-define( 'WP_DEBUG', true );
-define( 'SCRIPT_DEBUG', true );
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
 PHP
 fi
 
@@ -53,9 +60,25 @@ else
   noroot wp core update --version="${WP_VERSION}"
 fi
 
+# Create some directories
+if [[ ! -d "${VVV_PATH_TO_SITE}/import" ]]; then
+    echo "Creating Import directory..."
+    mkdir -p import
+fi
+if [[ ! -d "${VVV_PATH_TO_SITE}/export" ]]; then
+    echo "Creating Export directory..."
+    mkdir -p export
+fi
+if [[ ! -d "${VVV_PATH_TO_SITE}/release" ]]; then
+    echo "Creating Release directory..."
+    mkdir -p release
+fi
+
 cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+sed -i "s#{{WEBROOT_HERE}}#${WEBROOT}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 
+# SSL
 if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
     sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
     sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
