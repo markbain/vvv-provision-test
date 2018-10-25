847177ead3b35f6fdf8a2fc68cec163aa65db104
markbain <markbaindesign@gmail.com>
2018-10-25 18:56:36 +0200

    include scripts in provision

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -63,20 +63,8 @@ else
   noroot wp core update --version="${WP_VERSION}" --path="${VVV_PATH_TO_SITE}/${WEBROOT}"
 fi
 
-
-cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-sed -i "s#{{WEBROOT_HERE}}#${WEBROOT}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-
-# SSL
-if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
-    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-else
-    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-fi
-
+source "${scripts_dir}/ssl.sh"
+source "${scripts_dir}/nginx.sh"
 source "${scripts_dir}/create_dirs.sh"
 source "${scripts_dir}/import.sh"
 source "${scripts_dir}/replace.sh"
