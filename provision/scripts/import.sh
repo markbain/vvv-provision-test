670c3a3cef6b7d1e4a72963ab02866ba691dbcf4
markbain <markbaindesign@gmail.com>
2018-10-26 10:30:53 +0200

    fix bug

--- provision/scripts/import.sh
+++ provision/scripts/import.sh
@@ -3,10 +3,12 @@
 # Import existing site or default content
 
 if [[ -d "${archives_files}" ]]; then
+	echo -e "\n Importing archive...\n\n"
   unzip "{$archives_files}" -d "${VVV_PATH_TO_SITE}/${WEBROOT}"
 fi
 if [[ -d "$archives_db" ]]; then
-   unzip $db -d "${VVV_PATH_TO_SITE}/${WEBROOT}"
-   wp db import db
-   wp search-replace "${old_domain}" "${DOMAIN}" --all-tables
+	echo -e "\n Importing database...\n\n"
+ 	unzip $db -d "${VVV_PATH_TO_SITE}/${WEBROOT}"
+ 	wp db import db
+ 	wp search-replace "${old_domain}" "${DOMAIN}" --all-tables
 fi
