796bd37d2ff49ff884b82e8b73503792643521b2
markbain <markbaindesign@gmail.com>
2018-10-26 16:10:31 +0200

    bug fix

--- provision/scripts/import.sh
+++ provision/scripts/import.sh
@@ -3,17 +3,25 @@
 # Import existing site or default content
 
 echo -e "\nChecking for existing site to import...\n\n"
+echo -e "\n...\n\n"
+echo -e "\n...\n\n"
 
-if [[ -f "${archives_files}" ]]; then
+if [[ -f "${archive_files}" ]]; then
 	echo -e "\n Importing archive...\n\n"
-  unzip "{$archives_files}" -d "${VVV_PATH_TO_SITE}/${WEBROOT}"
+	echo -e "\n...\n\n"
+	echo -e "\n...\n\n"
+  unzip "{$archive_files}" -d "${VVV_PATH_TO_SITE}/${WEBROOT}"
 
 else 
 	echo -e "\nNo files found to import\n\n"
 fi
-if [[ -f "$archives_db" ]]; then
+if [[ -f "$archive_db" ]]; then
 	echo -e "\n Importing database...\n\n"
+	echo -e "\n...\n\n"
+	echo -e "\n...\n\n"
  	unzip $db -d "${VVV_PATH_TO_SITE}/${WEBROOT}"
  	wp db import db
  	wp search-replace "${old_domain}" "${DOMAIN}" --all-tables
+else 
+	echo -e "\nNo database found to import\n\n"
 fi
