72948e11939488f90ccfde5bbc9666be584c46b2
markbain <markbaindesign@gmail.com>
2018-10-26 15:48:32 +0200

    bug fix

--- provision/scripts/webroot.sh
+++ provision/scripts/webroot.sh
@@ -2,7 +2,7 @@
 
 # Define webroot
 echo -e "\nDefining webroot for ${VVV_SITE_NAME}..."
-if [[ ! -f "/vagrant/www/custom-configs/${VVV_SITE_NAME}/${VVV_SITE_NAME}.conf" ]]; then
+if [[ -f "/vagrant/www/custom-configs/${VVV_SITE_NAME}/${VVV_SITE_NAME}.conf" ]]; then
   source "/vagrant/www/custom-configs/${VVV_SITE_NAME}/${VVV_SITE_NAME}.conf"
 else
 	echo -e "\nNo custom webroot found in /vagrant/www/custom-configs/${VVV_SITE_NAME}/${VVV_SITE_NAME}.conf"
